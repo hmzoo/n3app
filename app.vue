f383adc4489b62366c3012f914d502be392f5528
HMJ <hmj@goatpage.com>
2022-06-22 07:35:01 +0200

    ok

--- app.vue
+++ app.vue
@@ -1,22 +1,55 @@
 <script setup>
-
 </script>
 
 <template>
-  <nav>
-    <i-button color="primary" size=lg >Primary <nuxt-icon name="settings"/></i-button>
-    <div style="color:brown">
-      <nuxt-icon name="settings" style="font-size:20em"/>
-    </div>
-  </nav>
+  <i-navbar>
+    <i-navbar-brand to="/">
+      <div class="title">Navbar</div>
+    </i-navbar-brand>
+
+    <i-navbar-collapsible>
+      <i-nav>
+        <i-nav-item > Home </i-nav-item>
+      </i-nav>
+      <i-nav>
+        <i-nav-item > OK </i-nav-item>
+      </i-nav>
+    </i-navbar-collapsible>
+    <i-nav-item > OK </i-nav-item>
+    <i-nav-item>
+      <i-button><nuxt-icon name="settings" /></i-button>
+    </i-nav-item>
+  </i-navbar>
 </template>
 
 <style lang="scss">
-
+@import '@inkline/inkline/css/variables';
+@import '@inkline/inkline/css/mixins';
 :root {
+  --body--background:color(#FF0000)
       --color--primary--h: 195deg;
       --color--primary--s: 77%;
       --color--primary--l: 39%;
+}
+
+.title {
+  color:white;
+  margin:0;
+  font-size:3em;
+  font-weight:900;
+  margin:0;
+  padding:0;
+}
+
+
+@include i-navbar() {
+      
+         @include variant('light') {
+
+        ----background:steelblue;
+        ----item--color:white;
+       
+        }
 }
 </style>
 
