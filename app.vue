23780a3b727e753eb4eae7a03cf21ab25abc9741
HMJ <hmj@goatpage.com>
2022-06-23 23:41:12 +0200

    ok

--- app.vue
+++ app.vue
@@ -1,55 +1,139 @@
 <script setup>
+
+const sidebar=ref(false)
+
+const switchSideBar=()=>{
+  console.log("OK")
+  sidebar.value=!sidebar.value
+}
+
+onMounted(() => {
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#edeff5",
+      "text": "#838391"
+    },
+    "button": {
+      "background": "#4b81e8"
+    }
+  }
+});
+});
+
+
 </script>
 
 <template>
-  <i-navbar>
-    <i-navbar-brand to="/">
-      <div class="title">Navbar</div>
+<i-layout>
+<i-layout-header>
+  <i-navbar fluid >
+    <i-navbar-brand>
+      <i-nav style="margin-right:20px">
+      <span class="title">OOCAM</span>
+      </i-nav>
+       <i-nav>
+      <i-nav-item>
+        <nuxt-icon name="EnabCam" class="iconbtn" />
+      </i-nav-item>
+      <i-nav-item>
+        <nuxt-icon name="EnabMic" class="iconbtn" />
+      </i-nav-item>
+      <i-nav-item>
+        <nuxt-icon name="settings" class="iconbtn" @click="switchSideBar" />
+      </i-nav-item>
+     <i-nav-item>
+    <i-hamburger-menu  animation="arrow-left" />
+    </i-nav-item>
+    </i-nav>
+ 
     </i-navbar-brand>
 
-    <i-navbar-collapsible>
+    <i-navbar-collapsible  class="_justify-content:flex-end">
       <i-nav>
-        <i-nav-item > Home </i-nav-item>
-      </i-nav>
-      <i-nav>
-        <i-nav-item > OK </i-nav-item>
+      <i-input placeholder="Type something.." >
+        <template #append>
+          <i-button color="primary">
+            <nuxt-icon name="call" />
+          </i-button>
+        </template>
+      </i-input>
       </i-nav>
     </i-navbar-collapsible>
-    <i-nav-item > OK </i-nav-item>
-    <i-nav-item>
-      <i-button><nuxt-icon name="settings" /></i-button>
-    </i-nav-item>
+
   </i-navbar>
+</i-layout-header>
+
+
+    <i-layout vertical class="_padding-top:1/2">
+        <i-sidebar v-model="sidebar" :collapse="true" collapse-position="absolute">
+            <i-nav vertical>
+                <i-nav-item to="/">
+                    Home
+                </i-nav-item>
+                <i-nav-item to="/about">
+                    Contact
+                </i-nav-item>
+                <i-nav-item to="/contact">
+                    Contact
+                </i-nav-item>
+            </i-nav>
+        </i-sidebar>
+        <i-layout-content>
+            <i-container fluid>
+                <i-row>
+                    <i-column>
+                        <h1>
+                            Sidebar
+                        </h1>
+                        <p>
+                            Example content for a page with a sidebar that collapses on large screens, a layout typically seen in Documentation pages and Web Application dashboards.
+                        </p>
+                    </i-column>
+                </i-row>
+            </i-container>
+        </i-layout-content>
+    </i-layout>
+  </i-layout>
 </template>
 
 <style lang="scss">
-@import '@inkline/inkline/css/variables';
-@import '@inkline/inkline/css/mixins';
-:root {
-  --body--background:color(#FF0000)
-      --color--primary--h: 195deg;
-      --color--primary--s: 77%;
-      --color--primary--l: 39%;
-}
+@import url("https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css");
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@900&family=Rubik+Moonrocks&family=Secular+One&display=swap");
+@import "@inkline/inkline/css/variables";
+@import "@inkline/inkline/css/mixins";
 
 .title {
-  color:white;
-  margin:0;
-  font-size:3em;
-  font-weight:900;
-  margin:0;
-  padding:0;
+  font-family: "Secular One", sans-serif;
+  color: white;
+  font-size: 2.5em;
+  font-weight: 900;
+  margin: 0;
+  padding: 0;
 }
+
+.iconbtn {
+  font-size: 2em;
+  margin: 0;
+  padding: 0;
+  cursor: pointer;
+  color: white;
+}
+
 
 
 @include i-navbar() {
-      
-         @include variant('light') {
 
-        ----background:steelblue;
-        ----item--color:white;
-       
-        }
+  ----item--color: white;
+  ----collapsed--item--color: green;
+  ----collapsed--background: green;
+
+  @include variant("light") {
+      ----padding: 0;
+  ----margin:0;
+    
+    ----background: rgb(118, 167, 207);
+  }
 }
 </style>
 
