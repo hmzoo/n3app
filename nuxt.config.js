d3ecf0055062e4fed1125cbbfc20961bf4ea3539
HMJ <hmj@goatpage.com>
2022-06-23 23:41:05 +0200

    ok

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,13 +2,19 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-modules: [
+    meta: {
+        script: [
+            {
+                src: "https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js",
+                body: true,
+            }]
+    },
+    modules: [
         '@inkline/nuxt',
         'nuxt-icons'
     ],
     inkline: {
-        css :true
+        css: true
         // Plugin options (optional)
     }
 
